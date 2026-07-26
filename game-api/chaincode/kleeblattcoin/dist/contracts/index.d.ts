c1933926999b61325b7b729324485390700a15a8
Frederik Peter <frederik.peter@kleeblatt.games>
2026-07-27 00:12:44 +0200

    weitere

---
+++ game-api/chaincode/kleeblattcoin/dist/contracts/index.d.ts
@@ -0,0 +1,4 @@
+import { KleeblattCoinContract } from "./KleeblattCoinContract";
+export { KleeblattCoinContract } from "./KleeblattCoinContract";
+export declare const contracts: (typeof KleeblattCoinContract)[];
+//# sourceMappingURL=index.d.ts.map
